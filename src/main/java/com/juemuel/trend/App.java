8d3809d9da2f297cf476eaac95628dbec3afde26
Juemuel <799525644@qq.com>
2024-07-01 17:44:25 +0800

    update

---
+++ src/main/java/com/juemuel/trend/App.java
@@ -0,0 +1,13 @@
+package com.juemuel.trend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
