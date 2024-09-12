f49148ba6012a06c96ef16a24dac860b59aa43e9
Juemuel <52780296+juemuel@users.noreply.github.com>
2024-09-13 02:54:25 +0800

    add rabbitMQ

--- trend-trading-backtest-view/src/main/java/com/juemuel/trend/TrendTradingBackTestViewApplication.java
+++ trend-trading-backtest-view/src/main/java/com/juemuel/trend/TrendTradingBackTestViewApplication.java
@@ -28,12 +28,22 @@ public class TrendTradingBackTestViewApplication {
         int port = 0;
         int defaultPort = 8041;
         int eurekaServerPort = 8761;
+        int configServerPort = 8060;
+        int rabbitMQPort = 5672;
+
+        if(NetUtil.isUsableLocalPort(rabbitMQPort)) {
+            System.err.printf("检查到端口%d 未启用，判断 rabbitMQ 服务器没有启动，本服务无法使用，故退出%n", rabbitMQPort );
+            System.exit(1);
+        }
 
         if(NetUtil.isUsableLocalPort(eurekaServerPort)) {
             System.err.printf("检查到端口%d 未启用，判断 eureka 服务器没有启动，本服务无法使用，故退出%n", eurekaServerPort );
             System.exit(1);
         }
-
+        if(NetUtil.isUsableLocalPort(configServerPort)) {
+            System.err.printf("检查到端口%d 未启用，判断 config 服务器没有启动，本服务无法使用，故退出%n", configServerPort );
+            System.exit(1);
+        }
         if(null!=args && 0!=args.length) {
             for (String arg : args) {
                 if(arg.startsWith("port=")) {
