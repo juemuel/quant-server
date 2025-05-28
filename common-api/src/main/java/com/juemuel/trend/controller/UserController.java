9b8241a7e1e825b2fb2d12421c1fb4420e6bdb59
Juemuel <52780296+juemuel@users.noreply.github.com>
2025-05-29 00:20:18 +0800

    优化userController的返回结果

--- common-api/src/main/java/com/juemuel/trend/controller/UserController.java
+++ common-api/src/main/java/com/juemuel/trend/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.juemuel.trend.controller;
 
 import cn.hutool.json.JSONObject;
+import com.juemuel.trend.http.Result;
 import com.juemuel.trend.pojo.User;
 import com.juemuel.trend.service.UserService;
 import com.juemuel.trend.util.TokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @CrossOrigin(allowCredentials = "true")  //支持跨域
@@ -18,20 +22,21 @@ public class UserController {
         return "hello world";
     }
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public JSONObject login(@RequestBody JSONObject jsonObject){
-        System.out.println("JSONObject: " + jsonObject);
+    public Result<Object> login(@RequestBody JSONObject jsonObject){
+//        System.out.println("JSONObject: " + jsonObject);
         User user = userService.login(jsonObject);
         //若正确返回200，若错误返回400
-        JSONObject result = new JSONObject();
         if(user == null){
-            result.put("state",400);
-        }else {
-            result.put("state",200);
-            // 返回携带token
-            String token = TokenUtil.sign(jsonObject.getStr("userName"));
-            System.out.println(token);
-            result.put("token",token);
+            return Result.error(400, "登录失败");
         }
-        return  result;
+        String token = TokenUtil.sign(jsonObject.getStr("userName"));
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("token", token);
+        data.put("id", user.getId());
+        data.put("userName", user.getUserName());
+        data.put("role", user.getRole()); // 可根据实际业务设置角色
+
+        return Result.success(data);
     }
 }
