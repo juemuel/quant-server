f0592a244bb890dd3ff30fd0e256ccbb3ecc4230
Juemuel <52780296+juemuel@users.noreply.github.com>
2025-06-09 02:09:13 +0800

    增加group curl

--- common-api/src/main/resources/bash/groupManage.bash
+++ common-api/src/main/resources/bash/groupManage.bash
@@ -56,7 +56,41 @@ curl -X POST 'http://localhost:8031/api/common-api/group/item/update' \
 curl -X GET "http://localhost:8031/api/common-api/group/list?userId=1&groupId=1001&keyword=腾讯" \
   -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoMCIsInVzZXJOYW1lIjoibHZqdmppZSIsImV4cCI6MTc0ODM2MDUwMn0.R8ARQmDH4-_kCiUibE4vTICX__YJ1HZvVthRFOyIr84" \
 
-# 根据关键词搜索分组内元素
-curl -X GET "http://localhost:8031/api/common-api/group/list?userId=1&groupId=1001&keyword=腾讯" \
-  -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoMCIsInVzZXJOYW1lIjoibHZqdmppZSIsImV4cCI6MTc0ODM2MDUwMn0.R8ARQmDH4-_kCiUibE4vTICX__YJ1HZvVthRFOyIr84" \
 
+
+
+# 阿里巴巴
+curl -X POST http://localhost:8031/api/common-api/group/item/add ^
+-H "Content-Type: application/json" ^
+-H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoMCIsInVzZXJOYW1lIjoibHZqdmppZSIsImV4cCI6MTc0ODM2MDUwMn0.R8ARQmDH4-_kCiUibE4vTICX__YJ1HZvVthRFOyIr84" ^
+-d "{\"groupId\": 1, \"itemName\": \"阿里巴巴\", \"customData\": {\"code\": \"09988\", \"market\": \"hk\"}, \"ownerId\": 1}"
+
+# 腾讯控股
+curl -X POST http://localhost:8031/api/common-api/group/item/add ^
+-H "Content-Type: application/json" ^
+-H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoMCIsInVzZXJOYW1lIjoibHZqdmppZSIsImV4cCI6MTc0ODM2MDUwMn0.R8ARQmDH4-_kCiUibE4vTICX__YJ1HZvVthRFOyIr84" ^
+-d "{\"groupId\": 1, \"itemName\": \"腾讯控股\", \"customData\": {\"code\": \"00700\", \"market\": \"hk\"}, \"ownerId\": 1}"
+
+# 苹果公司
+curl -X POST http://localhost:8031/api/common-api/group/item/add ^
+-H "Content-Type: application/json" ^
+-H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoMCIsInVzZXJOYW1lIjoibHZqdmppZSIsImV4cCI6MTc0ODM2MDUwMn0.R8ARQmDH4-_kCiUibE4vTICX__YJ1HZvVthRFOyIr84" ^
+-d "{\"groupId\": 1, \"itemName\": \"苹果公司\", \"customData\": {\"code\": \"AAPL\", \"market\": \"NASDAQ\"}, \"ownerId\": 1}"
+
+# 恒生指数
+curl -X POST http://localhost:8031/api/common-api/group/item/add ^
+-H "Content-Type: application/json" ^
+-H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoMCIsInVzZXJOYW1lIjoibHZqdmppZSIsImV4cCI6MTc0ODM2MDUwMn0.R8ARQmDH4-_kCiUibE4vTICX__YJ1HZvVthRFOyIr84" ^
+-d "{\"groupId\": 2, \"itemName\": \"恒生指数\", \"customData\": {\"code\": \"HSI\", \"market\": \"hk\"}, \"ownerId\": 1}"
+
+# 标普500
+curl -X POST http://localhost:8031/api/common-api/group/item/add ^
+-H "Content-Type: application/json" ^
+-H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoMCIsInVzZXJOYW1lIjoibHZqdmppZSIsImV4cCI6MTc0ODM2MDUwMn0.R8ARQmDH4-_kCiUibE4vTICX__YJ1HZvVthRFOyIr84" ^
+-d "{\"groupId\": 2, \"itemName\": \"标普500\", \"customData\": {\"code\": \"SPX\", \"market\": \"US\"}, \"ownerId\": 1}"
+
+# 日经225
+curl -X POST http://localhost:8031/api/common-api/group/item/add ^
+-H "Content-Type: application/json" ^
+-H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoMCIsInVzZXJOYW1lIjoibHZqdmppZSIsImV4cCI6MTc0ODM2MDUwMn0.R8ARQmDH4-_kCiUibE4vTICX__YJ1HZvVthRFOyIr84" ^
+-d "{\"groupId\": 2, \"itemName\": \"日经225\", \"customData\": {\"code\": \"N225\", \"market\": \"JP\"}, \"ownerId\": 1}"
