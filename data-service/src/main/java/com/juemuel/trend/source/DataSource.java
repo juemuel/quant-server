7c99b0122dc9d0b1772293e42d5e25d10eb51891
Juemuel <799525644@qq.com>
2025-06-09 17:28:21 +0800

    Update DataSource.java

--- data-service/src/main/java/com/juemuel/trend/source/DataSource.java
+++ data-service/src/main/java/com/juemuel/trend/source/DataSource.java
@@ -5,7 +5,21 @@ import com.juemuel.trend.pojo.IndexData;
 import java.util.List;
 
 public interface DataSource {
+    /**
+     * 获取指数列表
+     * @return
+     */
     List<Index> fetchIndexes();
+    /**
+     * 获取指数数据
+     * @param code
+     * @return
+     */
     List<IndexData> fetchIndexData(String code);
+
+    /**
+     *
+     * @return
+     */
     String getType();
 }
